ddcb53ae9f64c9e8c739376080f2716535d021c8
crazytoucan <alexanderfryan@gmail.com>
2021-03-03 13:18:53 -1000

    Compile

--- src/__tests__/typeCompatibility.spec.ts
+++ src/__tests__/typeCompatibility.spec.ts
@@ -5,10 +5,16 @@ interface State {
   foo: number;
 }
 
-// @ts-ignore
+function __unused(_value: any) {
+  // noop
+}
+
+// @ts-ignore since this function is never used
 function test() {
   // we actually only need this to compile, so there's no real "test" here
   const reduxStore: redux.Store<State> = createStore<State>((s: State) => s, {
     foo: 5,
   });
+
+  __unused(reduxStore);
 }
